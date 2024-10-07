70f1ed9f8409c90ebae1438ad7db433243d30cbb
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-10-07 14:23:13 +0600

    download button updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Banner from "../components/Banner";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
@@ -7,9 +8,10 @@ const Home = () => {
     return (
         <div>
             <Banner />
-            <a className="btn btn-outline" href="https://drive.google.com/file/d/1cjHobhsAiv1xHANvuAUGHiZPlTI2b5vK/view?usp=sharing" download="resume.pdf">
-                📥 Download CV
+            <a className="btn btn-outline my-20 flex justify-center" href="https://drive.google.com/file/d/1y2pgnRxkoue1rKAE2WG2OhZVqoNT_W1w/view?usp=sharing" download="resume.pdf">
+                📥 DOWNLOAD CV
             </a>
+            {/* <Link className="btn btn-outline my-20" href="https://drive.google.com/file/d/1cjHobhsAiv1xHANvuAUGHiZPlTI2b5vK/view?usp=sharing">Download CV</Link> */}
             {/* <button href='https://drive.google.com/file/d/1cjHobhsAiv1xHANvuAUGHiZPlTI2b5vK/view?usp=sharing' className="btn btn-outline">Download CV</button> */}
 
             <Skills />
